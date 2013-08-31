b8d783706c330114914634dc01ad272da0612a0c
Alexander Streng <metalen.la@gmail.com>
2013-08-31 10:00:57 +0200

    Initialisation of the project

---
+++ Server.h
@@ -0,0 +1,17 @@
+/*
+ * Server.h
+ *
+ *  Created on: Aug 31, 2013
+ *      Author: alexander
+ */
+
+#ifndef SERVER_H_
+#define SERVER_H_
+
+class Server {
+public:
+	Server();
+	virtual ~Server();
+};
+
+#endif /* SERVER_H_ */
